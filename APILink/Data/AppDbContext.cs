d049f5a835a35c3716425dae5b0ee491377d8820
BrunoFreschiAgger <bruno.freschi2@gmail.com>
2024-07-29 01:04:15 -0300

    create AppDbContext

---
+++ APILink/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using APILink.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILink.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<Todo> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+        optionsBuilder.UseSqlite(connectionString: "DataSource=app.db;Cache=Shared");
+}
