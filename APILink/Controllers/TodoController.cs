eeb9555b578469512f1053a16d6b158d1074a808
BrunoFreschiAgger <bruno.freschi2@gmail.com>
2024-07-30 23:37:30 -0300

    create

---
+++ APILink/Controllers/TodoController.cs
@@ -0,0 +1,18 @@
+﻿using APILink.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace APILink.Controllers;
+
+[Route("v1")]
+[ApiController]
+public class TodoController : ControllerBase
+{
+    [HttpGet]
+    [Route("todos")]
+    public async Task<IActionResult> Get([FromServices] AppDbContext context)
+    {
+        var todos = await context.Todos.AsNoTracking().ToListAsync();
+        return Ok(todos);
+    }
+}
