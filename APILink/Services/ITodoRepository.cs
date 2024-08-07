51ac6b46ff05b1072ffd65b573278142529f2608
BrunoFreschiAgger <brunofreschi@agger.com.br>
2024-08-07 17:16:40 -0300

    create

---
+++ APILink/Services/ITodoRepository.cs
@@ -0,0 +1,14 @@
+﻿using APILink.Data;
+using APILink.Models;
+using APILink.ViewModels;
+
+namespace APILink.Services;
+
+public interface ITodoRepository
+{
+    Task<List<Todo>> GetAsync();
+    Task<Todo> GetByIdAsync(int id);
+    Task<Todo> PostAsync(AppDbContext context, CreateTodoViewModel model);
+    Task<Todo> PutAsync(AppDbContext context, CreateTodoViewModel model, int id);
+    Task<Todo> DeleteAsync(AppDbContext context, int id);
+}
